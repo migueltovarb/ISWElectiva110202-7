eb2b09d261a8c5486e0ec49cf2645560a412d6c1
juanvizueteefajardo <juanvizuette58@gmail.com>
2025-05-09 16:59:48 -0500

    seven commit

--- Plataforma-Gestion-Inventario/Backend/AppInventario/settings.py
+++ Plataforma-Gestion-Inventario/Backend/AppInventario/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'AppInventario.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'Plataforma_Gestion_Inventario',
+        'USER':'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
 
