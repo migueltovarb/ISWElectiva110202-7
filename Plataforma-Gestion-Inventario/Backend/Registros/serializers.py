36e0e8da4b5f6258e394658e817ce266986ab426
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-14 20:38:38 -0500

    Add CI/CD workflow for Azure deployment

--- Plataforma-Gestion-Inventario/Backend/Registros/serializers.py
+++ Plataforma-Gestion-Inventario/Backend/Registros/serializers.py
@@ -16,24 +16,11 @@ class EstadoEjecucionSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class MovimientoStockSerializer(serializers.ModelSerializer):
-    producto = ProductoSerializer(read_only=True)
-    producto_id = serializers.PrimaryKeyRelatedField(queryset=Producto.objects.all(), source='producto', write_only=True)
-    
-    tipo_movimiento = TipoMovimientoSerializer(read_only=True)
-    tipo_movimiento_id = serializers.PrimaryKeyRelatedField(queryset=TipoMovimiento.objects.all(), source='tipo_movimiento', write_only=True)
-    
-    usuario = UsuarioSerializer(read_only=True)
-    usuario_id = serializers.PrimaryKeyRelatedField(queryset=Usuario.objects.all(), source='usuario', write_only=True)
-    
-    estado = EstadoEjecucionSerializer(read_only=True)
-    estado_id = serializers.PrimaryKeyRelatedField(queryset=EstadoEjecucion.objects.all(), source='estado', write_only=True)
-    
+    producto = serializers.PrimaryKeyRelatedField(queryset=Producto.objects.all())
+    tipo_movimiento = serializers.PrimaryKeyRelatedField(queryset=TipoMovimiento.objects.all())
+    usuario = serializers.PrimaryKeyRelatedField(queryset=Usuario.objects.all())
+    estado = serializers.PrimaryKeyRelatedField(queryset=EstadoEjecucion.objects.all())
+
     class Meta:
-       model = MovimientoStock
-       fields = [
-            'id', 'producto', 'producto_id',
-            'tipo_movimiento', 'tipo_movimiento_id',
-            'cantidad', 'fecha',
-            'usuario', 'usuario_id',
-            'estado', 'estado_id'
-       ]
+        model = MovimientoStock
+        fields = '__all__'
