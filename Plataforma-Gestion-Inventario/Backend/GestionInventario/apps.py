e34dff0b47e2c9be09203466f23bbdabc07dbaca
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-15 21:33:20 -0500

    thirty-three

--- Plataforma-Gestion-Inventario/Backend/GestionInventario/apps.py
+++ Plataforma-Gestion-Inventario/Backend/GestionInventario/apps.py
@@ -4,3 +4,11 @@ from django.apps import AppConfig
 class GestioninventarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'GestionInventario'
+
+
+class AppInventarioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AppInventario'
+    
+    def ready(self):
+        import GestionInventario.signals
