fff51a3d10120410ce4aedb77db39a40add73eac
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-23 12:37:00 -0500

    ten commit

--- back/crudtareas/urls.py
+++ back/crudtareas/urls.py
@@ -4,5 +4,4 @@ from .views import CrudTareasAPIView
 urlpatterns = [
     path('tareas/',CrudTareasAPIView.as_view(),name="registrar-tarea"),
     path('tareas/<int:pk>',CrudTareasAPIView.as_view(),name="lista-tareas")
-
 ]
---
+++ front/src/services/tareasServices.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const CrudTareaPost = async (tareaData) => {
+  try {
+    const response = await axios.post(`${API_URL}/crud/tareas/`, tareaData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Error al registrar la tarea",
+      err.response.data || err.message
+    );
+    throw error;
+  }
+};
+
+export const CrudTareaGet = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/crud/tareas/`);
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Error al obtener tareas",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
+  }
+};
+
+export const CrudTareaPut = async (pk, tareaData) => {
+  try {
+    const response = await axios.put(
+      `${API_URL}//crud/tareas/${pk}/`,
+      tareaData,
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Error al actualizar la tarea",
+      error.response ? error.response.data : error.message
+    );
+    return response.data;
+  }
+};
+
+export const CrudTareDelete = async (pk) => {
+  try {
+    const response = await axios.delete(`${API_URL}//crud/tareas/${pk}/`);
+    return response.data;
+  } catch (error) {
+    console.error(
+      "Error al eliminar la tarea",
+      error.response ? error.response.data : error.message
+    );
+    throw error;
+  }
+};
+
+export const BuscarTareaPorId = async (pk) => {
+  const response = await axios.get(`${API_URL}/crud/tareas/${pk}/`);
+  return response.data;
+};
