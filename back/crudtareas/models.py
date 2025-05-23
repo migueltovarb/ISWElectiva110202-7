69158b29862b9df729e4ac3edb9c42d31b614ed5
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-23 11:40:08 -0500

    Complete crud tareas

--- back/crudtareas/models.py
+++ back/crudtareas/models.py
@@ -8,3 +8,19 @@ class RegistroTareas(models.Model):
     
     def __str__(self):
         return self.nombre 
+    
+class ListaTareas(models.Model):
+    codigo = models.ForeignKey(RegistroTareas, on_delete=models.CASCADE, related_name="codigo_tarea")
+    nombre_tarea = models.ForeignKey(RegistroTareas, on_delete=models.CASCADE, related_name="nombre_tarea")
+    descripcion_tarea = models.ForeignKey(RegistroTareas, on_delete=models.CASCADE, related_name="descripcion_tarea")
+
+
+class ActualizacionTareas(models.Model):
+    tarea = models.ForeignKey(RegistroTareas,on_delete=models.CASCADE,related_name="actualizaciones")
+    nombre_actualizado = models.CharField(max_length=200, blank=True)
+    descripcion_actualizado = models.CharField(max_length=400,blank=True)
+    
+class EliminarTareas(models.Model):
+    tarea = models.ForeignKey(RegistroTareas, on_delete=models.CASCADE,related_name="eliminaciones")
+    fecha_eliminacion = models.DateField(auto_now_add=True)
+    
