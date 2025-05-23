2c299e8790378d86d8680f1a28a9950fd964303e
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-23 11:11:43 -0500

    first commit

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Inicio from "./pages/Inicio";
 import RegistroTareas from "./pages/RegistroTareas";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Routes>
-      <Route path="/inicio" element={<Inicio />}></Route>{" "}
-      <Route path="RegistroTareas" element={<RegistroTareas />}></Route>
+      <Route path="/" element={<Inicio />} />
+      <Route path="RegistroTareas" element={<RegistroTareas />} />
     </Routes>
   );
 }
--- front/src/pages/Inicio.jsx
+++ front/src/pages/Inicio.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Inicio() {
-  return <div></div>;
+  return <div>APP DE CRUD DE TAREAS</div>;
 }
