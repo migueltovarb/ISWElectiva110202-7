318af40b6b483f534cd374a5eff3e45d5b1af73e
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-23 11:23:57 -0500

    third commit

--- back/crudtareas/models.py
+++ back/crudtareas/models.py
@@ -5,4 +5,5 @@ class RegistroTareas(models.Model):
     codigo = models.IntegerField(blank=True, default=0)
     nombre = models.CharField(blank=True, max_length=200)
     descripcion = models.CharField(blank=True, max_length=400)
-   
+
+ 
