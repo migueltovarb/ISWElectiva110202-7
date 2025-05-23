e7a0d3e8ca4f1b3c8fba0e73f541c2aee3b58686
jhonkw9w8e <juan.munozpabon@campusucc.edu.co>
2025-05-23 11:21:20 -0500

    Update crud tareas

--- back/crudtareas/models.py
+++ back/crudtareas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class RegistroTareas(models.Model):
+    codigo = models.IntegerField(blank=True, default=0)
+    nombre = models.CharField(blank=True, max_length=200)
+    descripcion = models.CharField(blank=True, max_length=400)
+   
